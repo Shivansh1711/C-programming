f4ce19fad6e7463253003ff389879cd292ec5cdc
Shivansh <ojharishu000@gmail.com>
2023-02-15 20:11:36 +0530

    ENter number upto which u want ap

---
+++ arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i,a,d;
+    printf("Enter a number upto n terms : ");
+    scanf("%d",&n);
+    printf("Enter first number for AP : ");
+    scanf("%d",&a);
+    printf("Common differnce : ");
+    scanf("%d",&d);
+    for(i=a;i<n*d;i=i+d)
+    {
+        printf("%d ",i);
+    }
+}
